f6a906ef917948f39af79441f2ed2e41aad13858
tipo <writer1370@gmail.com>
2026-01-04 23:54:52 +0900

    worker KV api 테스트

--- docs/app.js
+++ docs/app.js
@@ -4,13 +4,9 @@ fetch('./audios.json')
     const list = document.getElementById('list');
 
     data.forEach(audio => {
-      const div = document.createElement('div');
+      const li = document.createElement('li');
       div.className = 'audio-card';
-      div.innerHTML = `
-        <h3>${audio.title}</h3>
-        <p>${audio.desc}</p>
-        <audio controls src="${audio.url}"></audio>
-        <a href="${audio.url}" download>⬇️ 다운로드</a>
+      div.innerHTML = `${audio.title}
       `;
 
       list.appendChild(div);
